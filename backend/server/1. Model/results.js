9e6c05829b4f14b99a2f324b685b9db85c2cbafe
“beata-dan” <beata.danilevskaya@gmail.com>
2026-05-19 23:31:40 +0100

    Completed model for results

--- backend/server/1. Model/results.js
+++ backend/server/1. Model/results.js
@@ -10,14 +10,38 @@ class Outcome {
 
     static async getAllByStudentId(studentId) {
         const response = await db.query("SELECT o.score, c.challenge_name, s.name FROM outcome as o JOIN student as s ON o.student_id = s.id JOIN challenge as c ON o.challenge_id = c.id WHERE s.id = $1;", [studentId])
-        if (response.rows.length != 1) {
+        if (response.rows.length === 0) {
             throw new Error("Unable to locate results for this student.")
         }
-        return new Outcome(response.rows[0])
+        return response.rows.map(result => new Outcome(result))
     }
 
-    static async getAverageScoreByStudentId() {}
-
-    static async getAverageScoreBySubjectIdByStudentId() {}
+    static async getAverageScoreByStudentId(studentId) {
+        const response = await db.query("SELECT AVG(o.score) as average_score FROM outcome as o JOIN student as s ON o.student_id = s.id WHERE s.id = $1;", [studentId])
+        if (response.rows.length === 0) {
+            throw new Error("Unable to locate results for this student.")
+        }
+        return response.rows[0]
+    }
+    
+    static async getAverageScoreBySubjectIdByStudentId(studentId, subjectId) {
+        const response = await db.query(
+            `SELECT sub.subject_name, AVG(o.score) AS average_score 
+            FROM outcome AS o 
+            JOIN student AS s 
+            ON o.student_id = s.id 
+            JOIN challenge AS c 
+            ON o.challenge_id = c.id 
+            JOIN subject AS sub 
+            ON c.subject_id = sub.id 
+            WHERE s.id = $1 AND sub.id = $2 
+            GROUP BY s.name, sub.subject_name;`,
+            [studentId, subjectId]
+        )
+        if (response.rows.length === 0) {
+            throw new Error("Unable to locate results for this student and subject.")
+        }
+        return response.rows[0]
+    }
 }
 
