d8a238e788e6b321fd319baf58c0abc5deecb416
Thomas Barkshire <thomasbarkshire@gmail.com>
2026-05-19 11:33:32 +0100

    Created login.js and register.js

---
+++ frontend/Register/Register.js
@@ -0,0 +1,30 @@
+document.getElementById("register-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: form.get("username"),
+            password: form.get("password")
+        })
+    }
+
+    const response = await fetch("http://localhost:3000/users/register", options);
+    const data = await response.json();
+
+    if (response.status == 201) {
+        window.location.assign("../Login/login.html");
+    } else {
+        alert(data.error);
+    }
+})
+
+document.getElementById("login-nav").addEventListener("click", () => {
+    window.location.assign("../Login/login.html")
+})
