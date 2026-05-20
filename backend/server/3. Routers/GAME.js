97114e5898c8db4dabd1c2054304a7c18f4fa3b3
ArshaanSindhwani <asindhwani08@gmail.comgit config --global user.name ArshaanSindhwani>
2026-05-20 11:06:26 +0100

    Added GAME.js

---
+++ backend/server/3. Routers/GAME.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const gameController = require("../2. Controller/Game");
+
+const gameRouter = Router();
+
+gameRouter.get("/", gameController.index);
+gameRouter.get("/:id", gameController.show);
+gameRouter.get("/:id/questions", gameController.showQuestions);
+
+module.exports = gameRouter;
