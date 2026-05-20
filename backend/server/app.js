827887ec02b82a7b7ee94558a2102b4369bd9b53
ArshaanSindhwani <asindhwani08@gmail.comgit config --global user.name ArshaanSindhwani>
2026-05-20 11:08:43 +0100

    Added gameRouter

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,27 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const logger = require("morgan")
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
-const loginRouter = require("./3. Routers/LOGIN")
-const resultsRouter = require("./3. Routers/RESULTS")
+const loginRouter = require("./3. Routers/LOGIN");
+const resultsRouter = require("./3. Routers/RESULTS");
+const gameRouter = require("./3. Routers/GAME");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 if (process.env.NODE_ENV !== "test") {
-  app.use(logger("dev"))
+  app.use(logger("dev"));
 }
 
 app.get("/", (req, res) => {
   res.send({
     message: "welcome",
-    description: "EduFun API"
-  })
-})
+    description: "EduFun API",
+  });
+});
 
-app.use("/login", loginRouter)
-app.use("/results", resultsRouter)
+app.use("/login", loginRouter);
+app.use("/results", resultsRouter);
+app.use("/game", gameRouter);
 
-module.exports = app
+module.exports = app;
