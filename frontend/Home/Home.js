045d3307c4ba5afa47692716bf9c883830d9a023
codemustafa <15mohamedm@gmail.com>
2026-05-20 10:35:54 +0100

    created home page and home page js

---
+++ frontend/Home/Home.js
@@ -0,0 +1,19 @@
+document.getElementById("user-nav").addEventListener("click", () => {
+    window.location.assign()
+})
+
+document.getElementById("classes-nav").addEventListener("click", () => {
+    window.location.assign()
+})
+
+document.getElementById("scores-nav").addEventListener("click", () => {
+    window.location.assign()
+})
+
+document.getElementById("game-1").addEventListener("click", () => {
+    window.location.assign("../Game/Game.html");
+});
+
+document.getElementById("logout-nav").addEventListener("click", () => {
+    window.location.assign("../Login/login.html");
+});
