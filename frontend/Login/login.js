3ded9357d95b0ff071415f9a122ca31e6308ac95
codemustafa <15mohamedm@gmail.com>
2026-05-20 11:39:38 +0100

    added username to local storage

--- frontend/Login/login.js
+++ frontend/Login/login.js
@@ -20,6 +20,7 @@ document.getElementById("login-form").addEventListener("submit", async (e) => {
 
     if (response.status == 200) {
         localStorage.setItem("token", data.token);
+        localStorage.setItem("username", form.get("username"));
         window.location.assign("../Home/Home.html");
       } else {
         alert(data.error);
