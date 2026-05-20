7e62ea661b98ee583e200067758ca23db39de981
ArshaanSindhwani <asindhwani08@gmail.comgit config --global user.name ArshaanSindhwani>
2026-05-20 16:51:08 +0100

    Added create function

--- backend/server/2. Controller/Results.js
+++ backend/server/2. Controller/Results.js
@@ -1,38 +1,57 @@
-const Outcome = require("../1. Model/results")
+const Outcome = require("../1. Model/results");
 
 async function show(req, res) {
-    try {
-        const studentId = req.params.id
-        const results = await Outcome.getAllByStudentId(studentId)
-        res.status(200).send({ data: results })
-    } catch (err) {
-        res.status(404).send({ error: err.message })
-    }
+  try {
+    const studentId = req.params.id;
+    const results = await Outcome.getAllByStudentId(studentId);
+    res.status(200).send({ data: results });
+  } catch (err) {
+    res.status(404).send({ error: err.message });
+  }
+}
+
+async function create(req, res) {
+  try {
+    const data = req.body;
+    const result = await Outcome.create(data);
+
+    res.status(201).send({
+      data: result,
+    });
+  } catch (err) {
+    res.status(400).send({
+      error: err.message,
+    });
+  }
 }
 
 async function showAverage(req, res) {
-    try {
-        const studentId = req.params.id
-        const average = await Outcome.getAverageScoreByStudentId(studentId)
-        res.status(200).send({ data: average })
-    } catch (err) {
-        res.status(404).send({ error: err.message })
-    }
+  try {
+    const studentId = req.params.id;
+    const average = await Outcome.getAverageScoreByStudentId(studentId);
+    res.status(200).send({ data: average });
+  } catch (err) {
+    res.status(404).send({ error: err.message });
+  }
 }
 
 async function showAverageBySubject(req, res) {
-    try {
-        const studentId = req.params.id
-        const subjectId = req.params.subjectId
-        const average = await Outcome.getAverageScoreBySubjectIdByStudentId(studentId, subjectId)
-        res.status(200).send({ data: average })
-    } catch (err) {
-        res.status(404).send({ error: err.message })
-    }
+  try {
+    const studentId = req.params.id;
+    const subjectId = req.params.subjectId;
+    const average = await Outcome.getAverageScoreBySubjectIdByStudentId(
+      studentId,
+      subjectId,
+    );
+    res.status(200).send({ data: average });
+  } catch (err) {
+    res.status(404).send({ error: err.message });
+  }
 }
 
 module.exports = {
-    show,
-    showAverage,
-    showAverageBySubject
-}
+  show,
+  create,
+  showAverage,
+  showAverageBySubject,
+};
