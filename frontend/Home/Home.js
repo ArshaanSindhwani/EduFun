a85e450e2a4aed22ccec77a0e2a5d3166737299c
codemustafa <15mohamedm@gmail.com>
2026-05-20 12:58:35 +0100

    added challengeId to home.js

--- frontend/Home/Home.js
+++ frontend/Home/Home.js
@@ -15,6 +15,7 @@ document.getElementById("scores-nav").addEventListener("click", () => {
 })
 
 document.getElementById("game-1").addEventListener("click", () => {
+    localStorage.setItem("challengeId, 1");
     window.location.assign("../Game/Game.html");
 });
 
