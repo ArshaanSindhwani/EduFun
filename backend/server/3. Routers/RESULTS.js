c79d3fbc215ca38bdb8d5b1cf8f3cd2caf8307a9
ArshaanSindhwani <asindhwani08@gmail.comgit config --global user.name ArshaanSindhwani>
2026-05-20 16:55:50 +0100

    Added POST route for results

--- backend/server/3. Routers/RESULTS.js
+++ backend/server/3. Routers/RESULTS.js
@@ -1,11 +1,15 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const resultsController = require("../2. Controller/Results")
+const resultsController = require("../2. Controller/Results");
 
-const resultsRouter = Router()
+const resultsRouter = Router();
 
-resultsRouter.get("/:id", resultsController.show)
-resultsRouter.get("/:id/average", resultsController.showAverage)
-resultsRouter.get("/:id/subject/:subjectId", resultsController.showAverageBySubject)
+resultsRouter.get("/:id", resultsController.show);
+resultsRouter.post("/", resultsController.create);
+resultsRouter.get("/:id/average", resultsController.showAverage);
+resultsRouter.get(
+  "/:id/subject/:subjectId",
+  resultsController.showAverageBySubject,
+);
 
-module.exports = resultsRouter
+module.exports = resultsRouter;
